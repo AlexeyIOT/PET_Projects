8f587e3a897f35d29be7a757bbe505a1b9ad760c
AlexeyIOT <a.zaluzhnyy24@gmail.com>
2023-12-09 17:37:02 +0300

    GuessTheNumber Game :)

---
+++ GuessTheNumber Game.py
@@ -0,0 +1,7 @@
+print("Hello mate, wanna play a Guess the number game?")
+print("1 - Yes   2 - No")
+choose_to_play = int(input())
+
+if choose_to_play == 2:
+    print("Bye, see you another time!")
+    exit()
